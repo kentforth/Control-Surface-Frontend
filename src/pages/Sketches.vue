a47c6539061630afc0983f68a33ec9e792780deb
kentforth <yashaar@mail.ru>
2020-09-07 16:41:15 +0700

    create adaptive layouts for sketches page

--- src/pages/Sketches.vue
+++ src/pages/Sketches.vue
@@ -3,9 +3,9 @@
     <Navigation/>
     <div class="content container">
       <div class="sketches-all">
-        <router-link to="/" class="sketch-item" v-for="sketch in sketches" :key="sketch">
+        <router-link to="/" class="sketch-item" v-for="(sketch, index) in sketches" :key="index">
           <h1>{{sketch.title}}</h1>
-          <img src="../assets/images/icons/arduino file.png" alt="arduino file">
+          <img src="../assets/images/icons/arduino file.png" alt="arduino file" class="file-img">
         </router-link>
 
       </div>
@@ -27,9 +27,42 @@
       return {
         sketches: [
           {
-            title: 'Some',
+            title: 'One Two',
             sketchUrl: '/categories/:sketches/:id'
-          }
+          },
+          {
+            title: 'Three',
+            sketchUrl: '/categories/:sketches/:id'
+          },
+          {
+            title: 'Three',
+            sketchUrl: '/categories/:sketches/:id'
+          },
+          {
+            title: 'Three',
+            sketchUrl: '/categories/:sketches/:id'
+          },
+          {
+            title: 'Three',
+            sketchUrl: '/categories/:sketches/:id'
+          },
+          {
+            title: 'Three',
+            sketchUrl: '/categories/:sketches/:id'
+          },
+          {
+            title: 'Three',
+            sketchUrl: '/categories/:sketches/:id'
+          },
+          {
+            title: 'Three',
+            sketchUrl: '/categories/:sketches/:id'
+          },
+          {
+            title: 'Three',
+            sketchUrl: '/categories/:sketches/:id'
+          },
+
         ],
         exampleName: this.$route.params.sketches
       }
@@ -46,10 +79,8 @@
   }
 
   .sketches {
-    margin: 0;
-    padding: 0;
     width: 100%;
-    height: 100vh;
+    height: calc(120vh);
   }
 
   h1 {
@@ -59,10 +90,11 @@
 
   .sketches-all {
     display: grid;
-    grid-gap: 2em;
+    grid-gap: 4em;
     max-width: 100%;
     margin-top: 4em;
     grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    margin-bottom: 2em;
   }
 
   .sketch-item {
@@ -70,6 +102,14 @@
     justify-content: center;
     grid-template-rows: 0.3fr 1fr;
     align-items: center;
+  }
+
+  .file-img {
+    transition: all 0.2s ease-in-out;
+  }
+
+  .sketch-item:hover > .file-img {
+    -webkit-filter: drop-shadow(0 0 15px #F2AC29);
   }
 
   .sketch-item h1{
@@ -80,5 +120,49 @@
     width: 100%;
   }
 
+/*MEDIA QUERIES*/
+  @media screen and (max-width: 1200px) {
+    .sketches {
+      width: 100%;
+      height: calc(180vh);
+    }
+  }
 
+  @media screen and (max-width: 900px) {
+    .sketches {
+      width: 100%;
+      height: calc(190vh);
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    .sketches {
+      width: 100%;
+      height: 100%;
+      padding-bottom: 2em;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    .sketches {
+      padding-top: 4em;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    .sketches-all {
+      grid-gap: 2em;
+    }
+  }
+
+  @media screen and (max-width: 393px) {
+
+    .sketches-all {
+      justify-items: center;
+    }
+
+    .sketch-item {
+      width: 80%;
+    }
+  }
 </style>
